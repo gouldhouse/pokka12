d1f0f947b3e7c0b1385d20330a879729481b72b6
gouldhouse <gouldyhousehome@gmail.com>
2026-08-24 14:28:13 +0900

    feature/1

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js というファイルを開いて書き込みます
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ['Syne', 'sans-serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+}
